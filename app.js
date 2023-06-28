dad7bebd8ceecd3e49d48fb68462adc7f18fe487
Luiz Fernando <109436975+luizbfernando@users.noreply.github.com>
2023-06-28 08:51:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("A aplicação está se conectando ao banco de dados")
+/*Mais um console log de teste */
+console.log ("Nova alteração que você não viu!")
