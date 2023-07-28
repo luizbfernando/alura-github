ec3c8bee12fa7859903520832ddb60707595146c
Luiz Fernando <luizfernandobrogiato73@gmail.com>
2023-07-28 09:10:32 -0300

    New changes

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log ("Rodando o sistema de gerenciamento de jogos")
+console.log ("A aplicação está se conectando ao banco de dados")
+/*Mais um console log de teste */
+console.log ("Nova alteração que você não viu!")
+console.log ("Enviando para o gitgub")
